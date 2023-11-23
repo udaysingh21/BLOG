0e26e84c49a053dce8c769563196c5b46f878bc1
uday <uvs2111@gmail.com>
2023-11-24 00:12:22 +0530

    initial commit

--- myblogsite/blog/views.py
+++ myblogsite/blog/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView,ListView,DetailView,CreateView,UpdateView,DeleteView
+from  blog.models import Post,Comment
+from django.utils import timezone
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+from blog.forms import PostForm,CommentForm
 
 # Create your views here.
+class AboutView(TemplateView):
+    template_name='about.html'
+
+class PostListView(ListView):
+    model=Post
+
+    def get_queryset(self):
+        # this allows me to use django's ORM to query the database
+        # __lte is a field lookup that means less than or equal to
+        # -published_date means order by published date in descending order
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+
+class PostDetailView(DetailView):
+    model=Post
+
+class CreatePostView(LoginRequiredMixin,CreateView):
+    login_url='/login/'
+    redirect_field_name='blog/post_detail.html'
+    form_class=PostForm
+    model=Post
+
+class PostUpdateView(LoginRequiredMixin,UpdateView):
+    login_url='/login/'
+    redirect_field_name='blog/post_detail.html'
+    form_class=PostForm
+    model=Post
+
+
+class PostDeteteView(LoginRequiredMixin,DeleteView):
+    model=Post
+    success_url=reverse_lazy('post_list')
+    # reverse_lazy is used because we don't want to execute the reverse function until the post is deleted
+
+class DraftListView(LoginRequiredMixin,ListView):
+    login_url='/login/'
+    redirect_field_name='blog/post_list.html'
+    model=Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
+        # __isnull is a field lookup that means is null ie post is not published yet
+        # order by created date in ascending order
